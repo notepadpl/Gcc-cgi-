e53a6bd57f43ce1df34799ce771ab56abac0fd98
notepadpl <przychodzien12@gmail.com>
2024-09-24 01:26:16 +0200

    Add files via upload

--- sdlkey.cpp
+++ sdlkey.cpp
@@ -1,23 +1,47 @@
-#include <SDL.h>
-#include <emscripten.h>
-#include <stdio.h>
-#include <string>
 
-// Globalna zmienna, która przechowuje wprowadzony tekst
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <string>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 std::string inputText = "";
 
 // Zmienna globalna, aby kontrolować czy program ma się zakończyć
 int running = 1;
+int main(int argc, char** argv) {
+  printf("hello, world!\n");
 
-// Główna funkcja obsługująca zdarzenia SDL
-void main_loop() {
-    SDL_Event event;
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Surface *screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
+
+#ifdef TEST_SDL_LOCK_OPTS
+  EM_ASM("SDL.defaults.copyOnLock = false; SDL.defaults.discardOnLock = true; SDL.defaults.opaqueFrontBuffer = false;");
+#endif
+
+  if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
+  for (int i = 0; i < 256; i++) {
+    for (int j = 0; j < 256; j++) {
+#ifdef TEST_SDL_LOCK_OPTS
+      // Alpha behaves like in the browser, so write proper opaque pixels.
+      int alpha = 255;
+#else
+      // To emulate native behavior with blitting to screen, alpha component is ignored. Test that it is so by outputting
+      // data (and testing that it does get discarded)
+      int alpha = (i+j) % 255;
+#endif
+      *((Uint32*)screen->pixels + i * 256 + j) = SDL_MapRGBA(screen->format, i, j, 255-i, alpha);
+    }
+  }
+  if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
+  SDL_Flip(screen);
+SDL_Event event;
     
     // Sprawdzamy wszystkie zdarzenia SDL
     while (SDL_PollEvent(&event)) {
         if (event.type == SDL_QUIT) {
             running = 0;
-            emscripten_cancel_main_loop(); // Zatrzymujemy pętlę gdy zamykamy aplikację
+            //emscripten_cancel_main_loop(); // Zatrzymujemy pętlę gdy zamykamy aplikację
         } else if (event.type == SDL_KEYDOWN) {
             // Obsługujemy naciśnięcie klawisza
             const char* key = SDL_GetKeyName(event.key.keysym.sym);
@@ -31,34 +55,10 @@ void main_loop() {
             printf("Current input: %s\n", inputText.c_str());  // Wyświetlamy aktualny stan zmiennej
         }
     }
+  printf("you should see a smoothly-colored square - no sharp lines but the square borders!\n");
+  printf("and here is some text that should be HTML-friendly: amp: |&| double-quote: |\"| quote: |'| less-than, greater-than, html-like tags: |<cheez></cheez>|\nanother line.\n");
 
-    // Dalsze operacje (np. renderowanie) mogą być tutaj
-    // W tej chwili mamy tylko obsługę wejścia z klawiatury
-}
+  SDL_Quit();
 
-int main(int argc, char* argv[]) {
-    // Inicjalizacja SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
-
-    // Tworzymy okno SDL
-    SDL_Window* window = SDL_CreateWindow("SDL Input Example", 
-                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-                          640, 480, SDL_WINDOW_SHOWN);
-    
-    if (!window) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
-
-    // Ustawienie pętli głównej Emscripten, która będzie cyklicznie wywoływać `main_loop`
-    emscripten_set_main_loop(main_loop, 0, 1);
-
-    // Zakończenie działania SDL i czyszczenie zasobów
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+  return 0;
 }
