4f053ed8ed022defdaa09f53b25a213db8b5adf3
notepadpl <przychodzien12@gmail.com>
2024-09-25 21:31:17 +0200

    Update tel.c

--- tel.c
+++ tel.c
@@ -32,8 +32,7 @@ rNrKYTX9g7z3RgJRmxWuGHbeu
 #include <fcntl.h>
  
 #include<stdio.h>                // printf
-#include<string.h>                // strlen
-#include<string>                // string
+#include<string.h>                // strle
 #include<sys/socket.h>            // socket
 #include<arpa/inet.h>            // inet_addr
 #include<netdb.h>                // hostenta
