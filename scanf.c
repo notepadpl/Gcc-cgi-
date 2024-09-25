c8ed829d1bde3bb31629f08c3077242a9c5cd2f2
notepadpl <przychodzien12@gmail.com>
2024-09-25 19:54:28 +0200

    Add files via upload

--- scanf.c
+++ scanf.c
@@ -1,14 +1,23 @@
-/* scanf example */
 #include <stdio.h>
 
-int main ()
-{
-  char scanflag [80];
-  int i;
 
-  printf ("Enter your family name: ");
-  scanf ("%s",scanflag);  
-  printf ("Mr. %s \n",scanflag);
+#define MAX_HOST_LEN 100
 
-  return 0;
+int main() {
+    char host[MAX_HOST_LEN];
+    int port;
+
+    printf("Podaj nazwę hosta: ");
+    scanf("%99s", host); // Ograniczamy długość wejścia do 99 znaków
+
+    printf("Podaj numer portu: ");
+    if (scanf("%d", &port) != 1) {
+        printf("Błąd podczas wprowadzania portu.\n");
+        return 1;
+    }
+
+    printf("Podany host: %s\n", host);
+    printf("Podany port: %d\n", port);
+
+    return 0;
 }
